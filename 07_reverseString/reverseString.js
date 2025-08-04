7c3d462fca4984cfa75fd760ac79b3a2921cdf8f
Gustav M. Spartan <221394996+gmsprtn@users.noreply.github.com>
2025-08-04 16:49:39 +0200

    Solve 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let result = "";
+    chars = text.split("");
+    for (let i = chars.length - 1; i >= 0; i--) {
+        result += chars[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
