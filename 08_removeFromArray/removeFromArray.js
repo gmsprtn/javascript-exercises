d4dc59501aebd27cfdd0fcbb0e5c7e5805e7a6e3
Gustav M. Spartan <221394996+gmsprtn@users.noreply.github.com>
2025-08-04 17:06:23 +0200

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 1; i < arguments.length; i++) {
+        const item = arguments[i];
+        for (let j = arr.length - 1; j >= 0; j--) {
+            if (arr[j] === item) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
