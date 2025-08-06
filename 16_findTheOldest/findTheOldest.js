6103b8a64a48201106552579c718b37be667ddc8
Gustav M. Spartan <221394996+gmsprtn@users.noreply.github.com>
2025-08-06 23:25:28 +0200

    Solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people = Array(...people);
+    people.sort((a, b) => calcAge(a) - calcAge(b));
+    return people[people.length - 1];
 };
+
+function calcAge(person) {
+    const birth = person.yearOfBirth;
+    const death = person.yearOfDeath;
+    if (death === undefined) {
+        return new Date().getFullYear() - birth;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
