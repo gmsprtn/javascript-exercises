954e8ef98d6e6a212841dae7a4214739fa4d3067
Gustav M. Spartan <221394996+gmsprtn@users.noreply.github.com>
2025-08-06 22:57:51 +0200

    Solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index < 0) {
+        return "OOPS";
+    }
+    const series = [0, 1];
+    for (let i = 2; i <= index; i++) {
+        series.push(series[i - 1] + series[i - 2]);
+    }
+    return series[index];
 };
 
 // Do not edit below this line
