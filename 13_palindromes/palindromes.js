1172bb4a08238afd157d209bbf56ec32e5fefad8
Gustav M. Spartan <221394996+gmsprtn@users.noreply.github.com>
2025-08-06 22:46:41 +0200

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const letters = text.toLowerCase().split("").filter((l) => (l >= "a" && l <= "z" || l >= "0" && l <= "9"));
+    for (let i = 0; i < Math.floor(letters.length / 2); i++) {
+        if (letters[i] !== letters[letters.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
